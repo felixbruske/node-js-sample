ece692ab770681320e5bafb47ef807883b439a05
felixbruske <felix.bruske@qsa-bruske.de>
2013-09-03 14:29:06 +0200

    response changed

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Under construction!');
 });
 
 var port = process.env.PORT || 5000;
